66edaf2871ba3b5e4edc079f95557e7ad9f80759
Dipansu088 <dipunrikun2004@gmail.com>
2026-07-12 15:58:26 +0530

    Initial Commit: Python Calculator

---
+++ main.py
@@ -0,0 +1,65 @@
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+def mod(a,b):
+    return a%b
+
+
+while True:
+    try:
+        a=float(input("Enter 1st number (int or float only): "))
+        b=float(input("Enter 2nd number (int or float only): "))
+        print(f"Which operation do u wanna perform?")
+        print('''
+            1. Add
+            2. Subtract
+            3. Multiply
+            4. Divide
+            5. Modulus(Remainder)
+            ''')
+    except ValueError:
+        print("Plz enter value only in integer or float.")
+
+    try:
+        choice=int(input(f"Enter ur choice here: "))
+        
+        if choice==1:
+            result=add(a,b)
+            print(f"The addition of {a} and {b} is {result}")
+
+        elif choice==2:
+            result=subtract(a,b)
+            print(f"The difference of {a} and {b} is {result}")
+            
+        elif choice==3:
+            result=multiply(a,b)
+            print(f"The product of {a} and {b} is {result}")
+            
+        elif choice==4:
+            result=divide(a,b)
+            print(f"The division of {a} and {b} is {result}")
+            
+        elif choice==5:
+            result=mod(a,b)
+            print(f"The remainder of {a} and {b} is {result}")
+
+        elif choice==5:
+            print("Thank you for visiting.")
+            break
+        
+        else:
+            print("Enter a valid choice...")
+            
+    except ZeroDivisionError:
+        print("Value is not defined...")
+    except ValueError:
+        print("Enter choices between 1-5!!!")
